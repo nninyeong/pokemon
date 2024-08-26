bbe2380a7402374921f4224c9aa15b08752d3f5a
minyeong <minyk0512@gmail.com>
2024-08-26 20:12:36 +0900

    [feat] 디테일 페이지 내에서도 추가/삭제할 수 있도록 수정

--- pokemon/src/shared/Router.jsx
+++ pokemon/src/shared/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import PokemonDetail from "../pages/PokemonDetail";
+import { Provider } from "react-redux";
+import store from "../redux/configStore";
 
 const PAGE_PATH = {
   Home: "/",
@@ -13,17 +15,16 @@ export const getPath = (page) => PAGE_PATH[page];
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={PAGE_PATH.Home} element={<Home />} />
-        <Route path={PAGE_PATH.Dex} element={<Dex />} />
-        <Route path={PAGE_PATH.PokemonDetail} element={<PokemonDetail />} />
-        <Route
-          path={PAGE_PATH.PokemonDetail + "/:id"}
-          element={<PokemonDetail />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={PAGE_PATH.Home} element={<Home />} />
+          <Route path={PAGE_PATH.Dex} element={<Dex />} />
+          <Route path={PAGE_PATH.PokemonDetail} element={<PokemonDetail />} />
+          <Route path={PAGE_PATH.PokemonDetail + "/:id"} element={<PokemonDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
