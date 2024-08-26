dc0e455b7cfc9be44f764d14ebf5762ceb791289
minyeong <minyk0512@gmail.com>
2024-08-26 20:58:16 +0900

    [feat] redux persist 사용해 local storage와 연동

--- pokemon/src/shared/Router.jsx
+++ pokemon/src/shared/Router.jsx
@@ -3,7 +3,9 @@ import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import PokemonDetail from "../pages/PokemonDetail";
 import { Provider } from "react-redux";
-import store from "../redux/configStore";
+import { store } from "../redux/persistReducer";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../redux/persistReducer";
 
 const PAGE_PATH = {
   Home: "/",
@@ -16,14 +18,16 @@ export const getPath = (page) => PAGE_PATH[page];
 const Router = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={PAGE_PATH.Home} element={<Home />} />
-          <Route path={PAGE_PATH.Dex} element={<Dex />} />
-          <Route path={PAGE_PATH.PokemonDetail} element={<PokemonDetail />} />
-          <Route path={PAGE_PATH.PokemonDetail + "/:id"} element={<PokemonDetail />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={PAGE_PATH.Home} element={<Home />} />
+            <Route path={PAGE_PATH.Dex} element={<Dex />} />
+            <Route path={PAGE_PATH.PokemonDetail} element={<PokemonDetail />} />
+            <Route path={PAGE_PATH.PokemonDetail + "/:id"} element={<PokemonDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
