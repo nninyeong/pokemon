234a2ca307b3636e438a823bbd6875fd550cb5b3
minyeong <minyk0512@gmail.com>
2024-09-12 03:54:05 +0900

    [refactor] useCallback 활용해 불필요한 리렌더링 방지 (이동할 경로가 바뀌지 않았음에도 리렌더링 과정에서 navHandler가 새로운 함수로 생성 -> 아래 Button에도 새로운 props 전달...이 이어질 수 있음)

--- pokemon/src/components/Button/NavButton.jsx
+++ pokemon/src/components/Button/NavButton.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { getPath } from "../../shared/Router.jsx";
 import Button from "./Button.jsx";
+import {useCallback} from "react";
 
 const NavButton = ({ page, children, ...props }) => {
   const navigate = useNavigate();
-  const navHandler = (page) => {
+
+  const navHandler = useCallback((page) => {
     const path = getPath(page);
     navigate(path);
-  };
+  }, [navigate]);
 
   return (
     <Button
